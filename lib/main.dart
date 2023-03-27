54104e437c2bd5755ca5d42f133f421902acb227
safvan <sajusajuptl@gmail.com>
2023-03-27 14:29:05 +0530

    add first cmt

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
        primarySwatch: Colors.indigo,
       ),
-      home:const MainScreen(),
+      home:const  MainScreen(),
     );
   }
 }
